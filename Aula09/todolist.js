ac0a5e1102b8894c4c947e773a6a39a2e62659f5
Jaine Francisco <jainesantana042@gmail.com>
2025-11-01 12:53:13 -0300

    Conteudo da aula do dia 01/11/2025

---
+++ Aula09/todolist.js
@@ -0,0 +1,30 @@
+const incluir = ()=>{
+    const li = document.createElement("li");
+    
+    const txtCmp = document.querySelector("#idtexto");
+    
+    li.textContent = txtCmp.value;
+    
+    const ul = document.getElementById("idlista");
+    ul.appendChild(li);
+}
+
+const remover = ()=>{
+    const lis = document.querySelectorAll("#idlista li");
+    const txtCmp = document.querySelector("#idtexto");
+    const ul = document.querySelector("#idlista");
+    
+    lis.forEach(it => {
+        if (it.textContent == txtCmp.value){
+            ul.removeChild(it); 
+        }
+    });
+}
+
+
+const btnAdicionar = document.querySelector("#idadicionar");
+const btnRemover = document.querySelector("#idremover");
+
+
+btnAdicionar.addEventListener('click',incluir);
+btnRemover.addEventListener('click',remover);
